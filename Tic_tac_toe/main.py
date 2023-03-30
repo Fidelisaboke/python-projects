e543142d24433c3038723621fcadc544d4ec9c48
fidelisaboke <fidelisaboke@gmail.com>
2023-03-30 19:51:16 +0300

    Updated main.py to fix 'softlock' in the game

--- Tic_tac_toe/main.py
+++ Tic_tac_toe/main.py
@@ -1,6 +1,19 @@
-#Main program to execute the functions in the tictactoe module
+# Written by: Fidel Agade Isaboke
+# Idea adopted from: Cisco Skills for All
+# Main program to execute the functions in the tictactoe module
 import tictactoe
 from time import sleep
+
+# Function to check remaining options
+def no_remaining_options(board):
+    if tictactoe.make_list_of_free_fields(board) == []:
+        print("It is a draw! Good game!")
+        global game_over
+        game_over = True
+        return True
+    else:
+        return False
+
 
 # Creating the board:
 board = tictactoe.fill_board()
@@ -15,27 +28,34 @@ Choose the following options:
 
 game_over = False
 
+# Creating an infinite loop to run the main statements of the program until an 'exit' condition is triggered:
 while True:
-    option = input("> ")
+    option = input("> ") # Input pointer
+    if option == '1': # 'Play' option
+        while not game_over: # The game will continue until the game_over condition is true.
 
-    if option == '1':
-        while not game_over:
+            # 'Wait' for the computer to 'think' and make a move:
             print("Waiting for computer...")
             tictactoe.draw_move(board)
             sleep(2)
-            tictactoe.display_board(board)
-            tictactoe.enter_move(board)
+
+            # If there are no remaining options for the player, call the game a 'draw':
+            if no_remaining_options(board):
+                break
+
+            tictactoe.display_board(board) # Display the board once the computer makes its mark, 'X'
+            tictactoe.enter_move(board) # The player's turn to make a move
             sleep(1)
-            tictactoe.display_board(board)
-            remaining_options = tictactoe.make_list_of_free_fields(board)
+            tictactoe.display_board(board) # Display the board once the player makes a move
             
             cpu_winner = tictactoe.victory_for(board, 'X')
             player_winner = tictactoe.victory_for(board, 'O')
             
-            if remaining_options == []:
-                print("It is a draw! Good game!")
-                game_over = True
+            # If there are no remaining options for the computer, call the game a 'draw':
+            if no_remaining_options(board):
                 break
+
+            # Announce the winner if there's one:
             elif cpu_winner:
                 print('Computer wins!')
                 game_over = True
@@ -45,7 +65,7 @@ while True:
                 game_over = True
                 break
 
-    elif option == '2':
+    elif option == '2': # 'Exit' option
         print('Closing program...')
         sleep(1)
         break
