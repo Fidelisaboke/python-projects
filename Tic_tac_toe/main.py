e74b9fe2eb1d9360c324a532c8301ca3624673d8
fidelisaboke <fidelisaboke@gmail.com>
2023-03-30 19:00:58 +0300

    Made changes to tictactoe and main file

--- Tic_tac_toe/main.py
+++ Tic_tac_toe/main.py
@@ -1,5 +1,53 @@
 #Main program to execute the functions in the tictactoe module
 import tictactoe
+from time import sleep
 
+# Creating the board:
 board = tictactoe.fill_board()
-tictactoe.display_board(board)
+
+# Welcome message:
+print("""
+Welcome to the tic-tac-toe game!
+Choose the following options:
+1 - Play
+2 - Exit
+""")
+
+game_over = False
+
+while True:
+    option = input("> ")
+
+    if option == '1':
+        while not game_over:
+            print("Waiting for computer...")
+            tictactoe.draw_move(board)
+            sleep(2)
+            tictactoe.display_board(board)
+            tictactoe.enter_move(board)
+            sleep(1)
+            tictactoe.display_board(board)
+            remaining_options = tictactoe.make_list_of_free_fields(board)
+            
+            cpu_winner = tictactoe.victory_for(board, 'X')
+            player_winner = tictactoe.victory_for(board, 'O')
+            
+            if remaining_options == []:
+                print("It is a draw! Good game!")
+                game_over = True
+                break
+            elif cpu_winner:
+                print('Computer wins!')
+                game_over = True
+                break
+            elif player_winner:
+                print('Player wins!')
+                game_over = True
+                break
+
+    elif option == '2':
+        print('Closing program...')
+        sleep(1)
+        break
+    else:
+        print("Enter a valid option.")
--- Tic_tac_toe/tictactoe.py
+++ Tic_tac_toe/tictactoe.py
@@ -1,4 +1,6 @@
 from random import randrange
+# Creating a tuple of the two signs, ('X' and 'O'):
+game_signs = ('X', 'O')
 
 # Creating the board and assigning all squares with null (None)
 def create_empty_board():
@@ -12,7 +14,6 @@ def fill_board():
     for row in board:
         for square in range(len(row)):
             row[square] = square_value
-            square += 1
             square_value +=1
     return board
 
@@ -34,3 +35,77 @@ def display_board(board):
     |       |       |       |
     +-------+-------+-------+
     """)
+
+# Function that handles the moves of the user
+def enter_move(board):
+        move_done = False
+        while True:
+            try:
+                move = int(input("Select a square to make a move: "))
+                if move < 1 or move > 9:
+                    print("Incorrect option. Try again.")
+                else:
+                    for row in board:
+                        for square in range(len(row)):
+                               if row[square] == move and row[square] not in game_signs:
+                                row[square] = 'O'
+                                move_done = True
+
+                                if move_done:
+                                    print("Move made successfully")
+                                    return
+            except ValueError:
+                print("Integers only!")
+            except TypeError:
+                print("Please enter an integer.")
+            except:
+                print("An unknown error has occured.")
+
+
+# Function that handles the moves of the computer
+def draw_move(board):
+    for row in board:
+        for square in range(len(row)):
+            if row[square] == 5:
+                row[square] = 'X'
+                return
+    while True:
+        move = randrange(1, 10)
+        for row in board:
+            for square in range(len(row)):
+                if row[square] == move and row[square] not in game_signs:
+                    row[square] = 'X'
+                    return
+
+# Producing a list of free_fields:
+def make_list_of_free_fields(board):
+    free_squares = []
+    for row in board:
+        for square in range(len(row)):
+            if row[square] not in game_signs:
+                free_squares.append(row[square])
+    return free_squares
+
+
+# Checking for a victory:
+def victory_for(board, sign):
+    # Specifying win conditions:
+    left_diagonal_triple = [board[0][0], board[1][1], board[2][2]]
+    right_diagonal_triple = [board[0][2], board[1][1], board[2][0]]
+    left_triple = [board[0][0], board[1][0], board[2][0]]
+    middle_triple = [board[0][1], board[1][1], board[2][1]]
+    right_triple = [board[0][2], board[1][2], board[2][2]]
+
+    # Checking a row for 3 sign elements
+    for row in board:
+        if all(square == sign for square in row):
+            return True
+        
+    # Checking if a column or diagonal contains three sign elements and returns True if a win condition is met.
+    # If no win conditions are met, the function returns False.
+    if any(all(square == sign for square in triple) for triple in 
+           [left_diagonal_triple, right_diagonal_triple, left_triple, middle_triple, right_triple]
+           ):
+        return True
+    else:
+        return False
